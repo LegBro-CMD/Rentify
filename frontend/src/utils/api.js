5a49b7ca0fd89cbf9d4b78b670e849962c978bf9
LegBro-CMD <artistlittlemaker@gmail.com>
2025-11-03 12:37:01 +0800

    update1

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? '' 
+      ? '/api' 
       : 'http://localhost:5000/api', // Local dev mode
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
